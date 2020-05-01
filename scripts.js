9bf1afee7c7a92832d0ccd1c685d2d940027ad95
frankie-njie <njie_frankie@yahoo.com>
2020-05-01 23:00:34 +0100

    some changes

--- scripts.js
+++ scripts.js
@@ -1,36 +1,41 @@
 const container = document.querySelector('.container');
 const gridSize = document.querySelector('#input');
-
-// let grid = document.createElement('div');
-// grid.textContent = 'This is the glorious text-content!';
-
-// container.appendChild(grid);
+const gridRow = document.querySelector('.gridRow');
+const gridCol = document.querySelector('.gridCol');
+const resizeBtn = document.querySelector('.button');
 
 
 // Create Grid 
 function createGrid(rows, cols) {
-    const grid = gridSize.value ** 2;
+    //const grid = gridSize.value ** 2;
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (let i = 0; i < (rows * cols); i++) {
         let grids = document.createElement("div");
-        grids.style.backgroundColor = '#333333';
+        let squareHeightAndWidth = 600 / Number(gridSize.value);
 
-        let squareHeightAndWidth = 600 / Number(gridSize.value)
+        grids.style.backgroundColor = '#333333';
         grids.style.height = squareHeightAndWidth + 'px'
         grids.style.width = squareHeightAndWidth + 'px'
-        grids.classList.add('square')
 
-        container.appendChild(grids).className = "grid-item";
+        container.appendChild(grids);
 
-
-        // let gridSize = 16
-        // for (let i = 0; i < gridSize; i++) {
-        //     let gridItem = document.createElement("div");
-        //     gridItem.style.cssText = 'background-color : blue; height : 20px; width : 20px';
-        //     gridItem.classList.add('grid-item')
-        //     container.appendChild(gridItem);
     }
-
 }
-createGrid(16, 16);
+// createGrid(10, 10);
+
+function resizeGrid() {
+    let newGridRow = document.querySelector('.gridRow').value;
+    let newGridCol = document.querySelector('.gridCol').value;
+
+    resetGrid();
+    createGrid(newGridCol, newGridRow);
+}
+
+function resetGrid() {
+    let oldGrid = document.querySelector('.container');
+    oldGrid.innerHTML = "";
+}
+
+// button events listeners
+resizeBtn.addEventListener('click', resizeGrid);
